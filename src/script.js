8affc2765aad226dbb43e97648dcd2ac72d26a82
molkify 3 <robertbarcelay@gmail.com>
2024-12-29 01:03:13 -0500

    a low taper fade

--- src/script.js
+++ src/script.js
@@ -1,20 +1,2 @@
-// refresh proxy frame
+// typer
 
-function refreshFrame() {
-    const iframe = document.getElementById('proxyFrame');
-    iframe.src = iframe.src; 
-}
-// fullscreen
-function toggleFullscreen() {
-    const iframe = document.getElementById('proxyFrame');
-    if (iframe.requestFullscreen) {
-        iframe.requestFullscreen();
-    } else if (iframe.mozRequestFullScreen) { 
-        iframe.mozRequestFullScreen();
-    } else if (iframe.webkitRequestFullscreen) { 
-        iframe.webkitRequestFullscreen();
-    } else if (iframe.msRequestFullscreen) { 
-        iframe.msRequestFullscreen();
-    }
-}
-// end
