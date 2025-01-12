368cfd400fabda173f52fa7654b14408e5e1e5ba
molkify 3 <robertbarcelay@gmail.com>
2025-01-11 23:04:17 -0500

    Update mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -6,15 +6,15 @@ if (/bot|crawler|spider|crawling/i.test(userAgent)) {
 
 // Windows Phone
 else if (/windows phone/i.test(userAgent)) {
-  // window.location.replace("/mobile/index.html");
+  window.location.replace("/mobile/index.html");
 }
 
 // Android
 else if (/android/i.test(userAgent)) {
-  // window.location.replace("/mobile/index.html");
+  window.location.replace("/mobile/index.html");
 }
 
 // iOS
 else if ((/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) || (navigator.maxTouchPoints && navigator.maxTouchPoints > 2 && /MacIntel/.test(navigator.platform))) {
-  // window.location.replace("/mobile/index.html");
+  window.location.replace("/mobile/index.html");
 }
